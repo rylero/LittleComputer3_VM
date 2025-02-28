616eb8d271396a4a96c1d9319c095ad540f33cdc
Ryan LeRoy <leroy.ryan09@gmail.com>
2025-02-27 23:28:44 -0800

    Followed https://www.jmeiners.com/lc3-vm/#:sign-extend

---
+++ CMakeFiles/VM_LC3.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VM_LC3.dir/src/main.c.o"
+  "CMakeFiles/VM_LC3.dir/src/main.c.o.d"
+  "VM_LC3"
+  "VM_LC3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/VM_LC3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/VM_LC3.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for VM_LC3.
+# This may be replaced when dependencies are built.
